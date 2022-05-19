a19223e09088d60d1a4b99c089eaa0e67d9cd2e6
Menno Wolvers <112menno@gmail.com>
2022-05-19 17:04:59 +0200

    Used the correct arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-cat $0 > /service-account.json
+echo $1 > /service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS="/service-account.json"
-export ENVIRONMENT=$1
+export ENVIRONMENT=$2
 
-gcloud container clusters get-credentials $2 --zone europe-west4-a --project $3
+gcloud container clusters get-credentials $3 --zone europe-west4-a --project $4
 
-sh -c " $4"
+sh -c " $5"
