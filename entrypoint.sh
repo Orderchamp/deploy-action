06bc7ee04271dfdf373c59c4e42f7f78ff92a278
Menno Wolvers <112menno@gmail.com>
2022-05-19 17:32:41 +0200

    Google cloud auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo $1 > /service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS="/service-account.json"
 export ENVIRONMENT=$2
 
+gcloud auth activate-service-account --key-file=/service-account.json
 gcloud container clusters get-credentials $3 --zone europe-west4-a --project $4
 
 sh -c " $5"
